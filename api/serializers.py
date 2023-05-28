421d8a7203ed8305bf74b5d11fb69aaa64c5ce92
Simex <osimiloluwa9@example.com>
2023-05-28 18:48:03 +0100

    merge with Goodlife

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from commute_share.models import *
 from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework.permissions import IsAuthenticated
 
 from djoser.serializers import UserCreateSerializer as CreateSerializer
 
 
-class CarModel(serializers.ModelSerializer):
+class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarModel
         fields = ['license_plate_number', 'identification_number', 'color']
@@ -66,3 +67,61 @@ class UserCreate(CreateSerializer):
 
     class Meta(CreateSerializer.Meta):
         fields = ['first_name', 'last_name', 'phone_number', 'username', 'email', 'password']
+
+
+# class DriverSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = DriverModel
+#         fields = ('name', 'car_model', 'license_number')
+#
+#     def validate_license_number(self, value):
+#         existing_driver = DriverModel.objects.filter(license_number=value).first()
+#         if existing_driver:
+#             raise serializers.ValidationError('A driver with this license number already exists.')
+#         return value
+#
+#     def create(self, validated_data):
+#         driver = DriverModel.objects.create(**validated_data)
+#         return driver
+
+
+class DriverSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+    verify_driver = VerificationSerializer
+    car = CarSerializer
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('driver')
+        user_serializer = UserSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+
+        verification_data = validated_data.pop('verification')
+        verification_serializer = VerificationSerializer(data=verification_data)
+        verification_serializer.is_valid(raise_exception=True)
+        verification = verification_serializer.save()
+
+        driver = DriverModel.objects.create(user=user, verification=verification, **validated_data)
+        return driver
+
+    class Meta:
+        model = DriverModel
+        fields = ('id', 'username', 'password', 'name', 'car_model', 'license_number')
+
+
+class DriverLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class RideSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideModel
+        fields = ('id', 'driver', 'source', 'destination', 'departure_time')
+
+    def create(self, validated_data):
+        validated_data['driver'] = self.context['request'].user
+        ride = RideModel.objects.create(**validated_data)
+        return ride
+
+
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 from commute_share.models import *
 from .permissions import *
@@ -11,4 +15,57 @@ class PassengerViewSet(ModelViewSet):
     serializer_class = PassengerSerializer
     permission_classes = [IsUser]
 
+<<<<<<< HEAD
 
+=======
+    def get_queryset(self):
+        m_user = self.request.user
+        if not m_user.is_authenticated:
+            raise PermissionDenied("You must be authenticated to access this resource.")
+        print(m_user.email)
+        queryset = PassengerModel.objects.filter(user__first_name=m_user.first_name, user__last_name=m_user.last_name,
+                                                 user__email=m_user.email)
+        print(queryset)
+        return queryset
+
+
+# class DriverRegistrationView(generics.CreateAPIView):
+#     serializer_class = DriverSerializer
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_create(serializer)
+#         headers = self.get_success_headers(serializer.data)
+#         return Response({
+#             'message': 'Driver registered successfully.',
+#             'data': serializer.data
+#         }, status=201, headers=headers)
+#
+#     def perform_create(self, serializer):
+#         serializer.save()
+
+# views.py
+
+
+class DriverRegistrationView(generics.CreateAPIView):
+    serializer_class = DriverSerializer
+    queryset = DriverModel.objects.all()
+    permission_classes = [IsUser]
+
+
+# class DriverLoginView(generics.GenericAPIView):
+#     serializer_class = DriverLoginSerializer
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         driver = serializer.validated_data
+#         token, _ = Token.objects.get_or_create(user=driver)
+#         return Response({'token': token.key})
+
+
+class RideCreateView(generics.CreateAPIView):
+    serializer_class = RideSerializer
+    permission_classes = [IsAuthenticated]
+>>>>>>> 7e71e4b7e77efc03a864ea06ad4b763e917aad48
